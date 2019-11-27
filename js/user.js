ba9bab9f49d444f77e3b898a7d0053fb4e233863
Jens-De-Muynck <jens.de.muynck@student.howest.be>
2019-11-27 23:13:58 +0100

    AddButtons

--- js/user.js
+++ js/user.js
@@ -1,21 +1,6 @@
-console.log('hello world')
-
-var firebaseConfig = {
-  apiKey: "AIzaSyCpGmPodLxoeFLh6Tz777qo_laPrlFjk68",
-  authDomain: "slimste-mens-fb04e.firebaseapp.com",
-  databaseURL: "https://slimste-mens-fb04e.firebaseio.com",
-  projectId: "slimste-mens-fb04e",
-  storageBucket: "slimste-mens-fb04e.appspot.com",
-  messagingSenderId: "424441270671",
-  appId: "1:424441270671:web:39e91bc6052ac1935abbfa",
-  measurementId: "G-XDPXY9C1VN"
-};
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
 function points() {
   let points = db.ref('teams');
-  points.on('vasue', function(snapshot) {
+  points.on('value', function (snapshot) {
     document.getElementById('teamOne').innerHTML = snapshot.val().teamOne.name
     document.getElementById('teamOneScore').innerHTML = snapshot.val().teamOne.points
     document.getElementById('teamTwo').innerHTML = snapshot.val().teamTwo.name
@@ -25,12 +10,10 @@ function points() {
   })
 }
 
-let db = firebase.database();
-
 let state = db.ref('state');
-state.on('value',function(snapshot) {
+state.on('value', function (snapshot) {
   let stateVal = snapshot.val()
-  if(stateVal == true){
+  if (stateVal == true) {
     points();
   } else {
     stateVal = snapshot.val()
